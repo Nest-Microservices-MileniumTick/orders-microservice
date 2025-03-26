6a95f3f2f2b5e4c5210173eab1445ed92dcce2ac
Josue Chavarria M <jchavarria@jchavarria.>
2025-03-25 23:44:16 -0600

    feat: se mejoró el servicio de pedidos para validar productos mediante un microservicio, se optimizó la creación de pedidos y se ajustó la recuperación de pedidos para incluir información de los productos asociados.

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,68 +1,162 @@
-import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
-import { RpcException } from '@nestjs/microservices';
-import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
 import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
+import { PRODUCT_SERVICE } from 'src/config/service';
+import { catchError, firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
-  private readonly logger = new Logger('OrdersService')
+  private readonly logger = new Logger('OrdersService');
+
+  @Inject(PRODUCT_SERVICE)
+  private readonly productsClient: ClientProxy;
 
   async onModuleInit() {
     await this.$connect();
-    this.logger.log('Database connected')
+    this.logger.log('Database connected');
   }
 
-  create(createOrderDto: CreateOrderDto) {
-    return this.order.create({ data: createOrderDto })
+  async create(createOrderDto: CreateOrderDto) {
+    const ids = createOrderDto.items.map((item) => item.productId);
+
+    const products: any[] = await firstValueFrom(
+      this.productsClient.send({ cmd: 'validate_products' }, { ids }).pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      ),
+    );
+
+    let totalItems = 0;
+    let totalAmount = 0;
+
+    for (const orderItem of createOrderDto.items) {
+      totalItems += orderItem.quantity;
+
+      const price = products.find(
+        (product) => product.id === orderItem.productId,
+      )?.price;
+
+      totalAmount += price * orderItem.quantity;
+    }
+
+    const order = await this.order.create({
+      data: {
+        totalAmount,
+        totalItems,
+        orderItems: {
+          createMany: {
+            data: products.map((product) => ({
+              productId: +product.id || 0,
+              quantity:
+                createOrderDto.items.find(
+                  (item) => item.productId === product.id,
+                )?.quantity || 0,
+              price: +product.price || 0,
+            })),
+          },
+        },
+      },
+      include: {
+        orderItems: {
+          select: {
+            productId: true,
+            quantity: true,
+            price: true,
+          },
+        },
+      },
+    });
+
+    return {
+      ...order,
+      orderItems: order.orderItems.map((orderItem) => ({
+        ...orderItem,
+        name: products.find((product) => product.id === orderItem.productId)
+          .name,
+      })),
+    };
   }
 
   async findAll(orderPaginationDto: OrderPaginationDto) {
-    const { page, limit, status } = orderPaginationDto
+    const { page, limit, status } = orderPaginationDto;
 
-    const records = await this.order.count({ where: { status } })
-    const lastPage = Math.ceil(records / limit)
+    const records = await this.order.count({ where: { status } });
+    const lastPage = Math.ceil(records / limit);
 
     return {
       data: await this.order.findMany({
         skip: (page - 1) * limit,
         take: limit,
-        where: { status }
+        where: { status },
       }),
       meta: {
         page,
         records,
-        lastPage
-      }
-    }
+        lastPage,
+      },
+    };
   }
 
   async findOne(id: string) {
     const order = await this.order.findUnique({
-      where: { id }
+      where: { id },
+      include: {
+        orderItems: {
+          select: {
+            productId: true,
+            quantity: true,
+            price: true,
+          },
+        },
+      },
     });
 
-    if (!order) throw new RpcException({
-      message: `Product with id #${id} not found`,
-      status: HttpStatus.NOT_FOUND
-    });
+    if (!order)
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
 
-    return order;
+    const ids = order.orderItems.map((item) => item.productId);
+
+    const products: any[] = await firstValueFrom(
+      this.productsClient.send({ cmd: 'validate_products' }, { ids }).pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      ),
+    );
+
+    return {
+      ...order,
+      orderItems: order.orderItems.map((orderItem) => ({
+        ...orderItem,
+        name: products.find((product) => product.id === orderItem.productId)
+          .name,
+      })),
+    };
   }
 
   async changeStatus(changeOrderStatusDto: ChangeOrderStatusDto) {
+    const { id, status } = changeOrderStatusDto;
 
-    const {id, status} = changeOrderStatusDto
+    const order = await this.findOne(id);
 
-    const order = await this.findOne(id)
-
-    if(order.status === status) return order;
+    if (order.status === status) return order;
 
     return this.order.update({
-      where: {id},
-      data: {status}
-    })
+      where: { id },
+      data: { status },
+    });
   }
 }
